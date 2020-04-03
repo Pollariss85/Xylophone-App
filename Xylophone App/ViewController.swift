bee994522c3bd3fb5db0f7a332a5ef8c9d911521
Robert Harrison <pollariss85@gmail.com>
2020-04-03 13:15:06 -0400

    Initial commit of Xylophone app to GitHub

--- Xylophone App/ViewController.swift
+++ Xylophone App/ViewController.swift
@@ -1,20 +1,51 @@
 //
 //  ViewController.swift
-//  Xylophone App
+//  Xylophone
 //
-//  Created by Robert Harrison on 3/31/20.
-//  Copyright © 2020 Robert Harrison. All rights reserved.
+//  Created by Angela Yu on 28/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        playSound(soundName: sender.currentTitle!)
+        
+        //Reduces the sender's (the button that got pressed) opacity to half.
+        sender.alpha = 0.5
+        
+        //Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
+        
+    }
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+       
+    }
+    
 }
+
+
+
+
+
+
+
 
